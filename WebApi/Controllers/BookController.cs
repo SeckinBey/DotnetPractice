8f82f9aa227bc330a68e78a809b32b2c6ce4c0e0
Seckin AKCA <38793483+SeckinBey@users.noreply.github.com>
2022-08-03 20:27:54 +0300

    Add HttpGet method

---
+++ WebApi/Controllers/BookController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]s")]
+    public class BookController : Controller
+    {
+        private static List<Book> BookList = new List<Book>()
+        {
+            new Book
+            {
+                Id = 1,
+                Title = "Lean Startup",
+                GenreId = 1, // Personal Growth
+                PageCount = 200,
+                PublishDate = new DateTime(2001, 06, 12)
+            },
+            new Book
+            {
+                Id = 2,
+                Title = "Herland",
+                GenreId = 2, // Science Fiction
+                PageCount = 250,
+                PublishDate = new DateTime(2010, 05, 21)
+            },
+            new Book
+            {
+                Id = 3,
+                Title = "Dune",
+                GenreId = 2, // Science Fiction
+                PageCount = 540,
+                PublishDate = new DateTime(2001, 12, 23)
+            }
+        };
+
+        [HttpGet]
+        public List<Book> GetBooks()
+        {
+            var bookList = BookList.OrderBy(x => x.Id).ToList<Book>();
+
+            return bookList;
+        }
+
+        [HttpGet("{id}")]
+        public Book GetBookById(int id)
+        {
+            var book = BookList.Where(x => x.Id == id).SingleOrDefault();
+            return book;
+        }
+    }
+}
